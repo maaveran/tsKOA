77fbaf55c9a9abbc296ce126a070885005ceedcd
maaveran <35824881+xbind@users.noreply.github.com>
2018-10-26 13:41:11 +0700

    add several patch update

--- src/router.js
+++ src/router.js
@@ -1,1 +1,17 @@
-import router from 'koa-router'
+
+import * as Router from 'koa-router';
+import User from './Controller/Learn';
+
+let apiRouter = new Router({
+    prefix: '/api'
+});
+
+apiRouter.get('/', async(ctx) => {
+    ctx.body = ctx.request.query
+});
+
+apiRouter.get('/user', async(ctx) => {
+    console.log(User(ctx));
+});
+
+export default apiRouter = apiRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,36 @@
-import * as Koa from 'koa'
-import * as Router from 'koa-router'
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as cors from 'koa-cors';
+import {Sequelize} from  'sequelize-typescript'
+
+import httpRoute from './router'
 
 const app = new Koa();
-const router =  new Router();
+const koaRouter = new Router();
 
-router.get('/*', async(ctx) => { 
-	ctx.body  = "hello world"
+const sequelize =  new Sequelize({
+	dialect  :  'postgres',
+	host 	 :  'localhost',
+	name 	 : 	'backend',
+	username : 	'postgres',
+	password :  'root',
+	modelPaths : [
+		__dirname + '/models'
+	]
 })
 
-app.use(router.routes());
+sequelize.sync({ force: true });
+app.use(koaRouter.routes());
+app.use(httpRoute.routes());
+
+koaRouter.get('/', async (ctx) => {
+    ctx.body = 'hello world';
+});
+
+app.use(cors({
+    origin: '*'
+}));
+
 
 app.listen(3000)
 
